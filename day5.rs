344a5296495ca26a1009aeb884426a30caeb60c5
Kirill Borisov <lensvol@gmail.com>
2015-12-10 22:06:18 +0300

    Solved Day 5 (Part 2).

--- day5.rs
+++ day5.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::io::{self, Read};
 
 fn read_from_stdin() -> String{
@@ -46,14 +47,49 @@ fn naughty_or_nice(input: String) -> Verdict{
     }
 }
 
-fn day5(input: String) -> i32 {
+fn naughty_or_nice_v2(input: String) -> Verdict {
+    let mut locations: HashMap<&str, usize> = HashMap::new();
+    let mut pair_found: bool = false;
+    let mut mirror_found: bool = false;
+    let mut previous_char: char = ' ';
+
+    for i in 0..input.len()-1 {
+        let part =  &input[i..i+2];
+        let end_char = part.chars().nth(1).unwrap();
+        if end_char == previous_char {
+            mirror_found = true;
+        }
+
+        match locations.insert(part, i) {
+            Some(prev_pos) => {
+                if prev_pos+1 != i {
+                    pair_found = true;
+                }
+            }
+            None => ()
+        }
+
+        previous_char = end_char;
+    }
+
+    if mirror_found && pair_found {
+        Verdict::Nice
+    } else {
+        Verdict::Naughty
+    }
+}
+
+fn day5<F>(input: &String, predicate: F) -> i32 where F: Fn(String) -> Verdict {
     let lines = input.lines();
     
-    let nice_verdicts: Vec<Verdict> = lines.map(|candidate| naughty_or_nice(candidate.to_string())).filter(|v| *v == Verdict::Nice).collect();
+    let nice_verdicts: Vec<Verdict> = lines.map(|candidate| predicate(candidate.to_string()))
+                                           .filter(|v| *v == Verdict::Nice)
+                                           .collect();
     nice_verdicts.len() as i32
 }
 
 fn main() {
     let input = read_from_stdin();
-    println!("Nice strings: {}", day5(input));
+    println!("Nice strings: {}", day5(&input, naughty_or_nice));
+    println!("Nice by new rules: {}", day5(&input, naughty_or_nice_v2));
 }
