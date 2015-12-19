be0629f62230afaddf779d438a7d628adb232172
Kirill Borisov <lensvol@gmail.com>
2015-12-19 17:34:45 +0300

    Completely forgot about part 2 of Day 1. Ugh.

--- day1.rs
+++ day1.rs
@@ -6,19 +6,35 @@ fn read_from_stdin() -> String {
     buffer
 }
 
-fn day1(input: String) -> i32 {
+fn day1(input: &String) -> (i32, Option<i32>) {
     let mut floor: i32 = 0;
+    let mut position: i32 = 0;
+    let mut basement_position: Option<i32> = None;
+    
     for c in input.chars() {
         floor = match c {
             '(' => floor + 1,
             ')' => floor - 1,
             _ => floor,
         };
+
+        position += 1;
+
+        if floor == -1 {
+            basement_position = match basement_position {
+                Some(pos) => Some(pos),
+                None => Some(position),
+            }
+        }
     }
-    floor
+    (floor, basement_position)
 }
 
 fn main() {
     let input = read_from_stdin();
-    println!("{}", day1(input));
+    let (ends_up_at, enters_basement_at) =  day1(&input);
+    println!("Ends up at {}", ends_up_at);
+    if let Some(position) = enters_basement_at {
+        println!("Enters basement at {}", position);
+    };
 }
