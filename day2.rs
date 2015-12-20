15276818a2cc87fac0285b49b3d6976960145c07
Kirill Borisov <lensvol@gmail.com>
2015-12-20 17:34:02 +0300

    ...And forgot about Day 2 Part 2, too.

--- day2.rs
+++ day2.rs
@@ -26,6 +26,25 @@ impl GiftBox {
         min(min(side1, side2), side3)
     }
 
+    pub fn smallest_perimeter(&self) -> i32 {
+        let side1 = (self.length + self.width) * 2;
+        let side2 = (self.width + self.height) * 2;
+        let side3 = (self.height + self.length) * 2;
+
+        min(min(side1, side2), side3)
+    }
+
+    pub fn smallest_distance(&self) -> i32 {
+        let distance1 = (self.length + self.width) * 2;
+        let distance2 = (self.height + self.width) * 2;
+        let distance3 = (self.length + self.height) * 2;
+        min(min(distance1, distance2), distance3)
+    }
+
+    pub fn volume(&self) -> i32 {
+        self.length * self.width * self.height
+    }
+
     pub fn new(dimensions: &str) -> GiftBox {
         let parts: Vec<i32> = dimensions.split("x").map(|s| s.parse::<i32>().unwrap()).collect();
         GiftBox {
@@ -36,17 +55,23 @@ impl GiftBox {
     }
 }
 
-fn day2(input: String) -> i32 {
+fn day2(input: &String) -> (i32, i32) {
     let lines = input.lines();
-
+    let mut total_paper: i32 = 0;
+    let mut total_ribbon: i32 = 0;
     let boxes: Vec<GiftBox> = lines.map(|dim| GiftBox::new(dim)).collect();
-    let paper_needed: Vec<i32> = boxes.iter()
-                                      .map(|b| b.surface_area() + b.smallest_side())
-                                      .collect();
-    paper_needed.iter().fold(0, |a, &b| a + b)
+
+    for b in boxes {
+        total_paper += b.surface_area() + b.smallest_side();
+        total_ribbon += min(b.smallest_perimeter(), b.smallest_distance()) + b.volume();
+    }
+    (total_paper, total_ribbon)
 }
 
 fn main() {
     let input = read_from_stdin();
-    println!("{}", day2(input));
+    let (paper, ribbon): (i32, i32) = day2(&input);
+    println!("Elves need {} square feet of wrapping paper and {} feet of ribbon ",
+             paper,
+             ribbon);
 }
